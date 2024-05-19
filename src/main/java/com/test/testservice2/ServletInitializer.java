589c37f09ab1538236e234655089ecd583945630
parishkar-nagrikar <p4parish@gmail.com>
2024-05-19 21:02:14 +0530

    initial commit

---
+++ src/main/java/com/test/testservice2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.test.testservice2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TestService2Application.class);
+	}
+
+}
---
+++ src/main/java/com/test/testservice2/TestService2Application.java
@@ -0,0 +1,13 @@
+package com.test.testservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestService2Application.class, args);
+	}
+
+}
